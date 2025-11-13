d28a2fdb42d89498a1d45c57626e164c9163446b
gentbunjaku71 <gentbunjaku71@gmail.com>
2025-11-13 21:14:44 +0100

    Update CartItem.js

--- CartItem.js
+++ CartItem.js
@@ -1,1 +1,21 @@
+import { useDispatch } from "react-redux";
+import { increaseQuantity, decreaseQuantity, removeItem } from "../redux/cartSlice";
+
+export default function CartItem({ item }) {
+  const dispatch = useDispatch();
+  return (
+    <div style={{ borderBottom: "1px solid #ccc", padding: "1rem", display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+      <img src={item.image} alt={item.name} width={80} />
+      <div>
+        <h4>{item.name}</h4>
+        <p>${item.price} x {item.quantity} = ${item.price * item.quantity}</p>
+      </div>
+      <div>
+        <button onClick={() => dispatch(increaseQuantity(item.id))}>+</button>
+        <button onClick={() => dispatch(decreaseQuantity(item.id))}>-</button>
+        <button onClick={() => dispatch(removeItem(item.id))}>Delete</button>
+      </div>
+    </div>
+  );
+}
 
