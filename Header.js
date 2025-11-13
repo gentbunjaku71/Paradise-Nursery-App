e6c8269b6dda7352848f61cbc92a564f6b21eaaa
gentbunjaku71 <gentbunjaku71@gmail.com>
2025-11-13 21:14:03 +0100

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,1 +1,17 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export default function Header() {
+  const totalQuantity = useSelector(state => state.cart.totalQuantity);
+  return (
+    <header style={{ display: "flex", justifyContent: "space-between", padding: "1rem", background: "#d4f0d4" }}>
+      <h2>Paradise Nursery</h2>
+      <nav>
+        <Link to="/" style={{ margin: "0 1rem" }}>Home</Link>
+        <Link to="/products" style={{ margin: "0 1rem" }}>Products</Link>
+        <Link to="/cart" style={{ margin: "0 1rem" }}>Cart ({totalQuantity})</Link>
+      </nav>
+    </header>
+  );
+}
 
