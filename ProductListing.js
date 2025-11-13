e67876676f7ad9a8ee1321d5a7f9c6f9f7075623
gentbunjaku71 <gentbunjaku71@gmail.com>
2025-11-13 21:15:11 +0100

    Update ProductListing.js

--- ProductListing.js
+++ ProductListing.js
@@ -1,1 +1,11 @@
+import { plants } from "../data/plants";
+import PlantCard from "../components/PlantCard";
+
+export default function ProductListing() {
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", padding: "2rem" }}>
+      {plants.map(plant => <PlantCard key={plant.id} plant={plant} />)}
+    </div>
+  );
+}
 
