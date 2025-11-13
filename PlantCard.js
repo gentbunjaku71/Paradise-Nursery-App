f8a8f70a619e63369328393749c2694b905b2e53
gentbunjaku71 <gentbunjaku71@gmail.com>
2025-11-13 21:14:19 +0100

    Update PlantCard.js

--- PlantCard.js
+++ PlantCard.js
@@ -1,1 +1,15 @@
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/cartSlice";
+
+export default function PlantCard({ plant }) {
+  const dispatch = useDispatch();
+  return (
+    <div style={{ border: "1px solid #ccc", padding: "1rem", margin: "1rem", textAlign: "center" }}>
+      <img src={plant.image} alt={plant.name} width={150} />
+      <h3>{plant.name}</h3>
+      <p>${plant.price}</p>
+      <button onClick={() => dispatch(addItem(plant))}>Add to Cart</button>
+    </div>
+  );
+}
 
