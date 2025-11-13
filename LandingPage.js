02baa3dbafc602af6fa8d6fd8830005c76d49326
gentbunjaku71 <gentbunjaku71@gmail.com>
2025-11-13 21:14:57 +0100

    Update LandingPage.js

--- LandingPage.js
+++ LandingPage.js
@@ -1,1 +1,20 @@
+import { Link } from "react-router-dom";
+
+export default function LandingPage() {
+  return (
+    <div style={{
+      height: "100vh",
+      background: "url('https://via.placeholder.com/1200x600') center/cover no-repeat",
+      color: "#fff",
+      textAlign: "center",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center"
+    }}>
+      <h1>Paradise Nursery</h1>
+      <p>Discover the best houseplants to bring life into your home!</p>
+      <Link to="/products"><button style={{ padding: "1rem 2rem", fontSize: "1.2rem" }}>Get Started</button></Link>
+    </div>
+  );
+}
 
