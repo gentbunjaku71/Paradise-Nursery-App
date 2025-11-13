43041b31ed94ca3e063fd749ceddd3eb30587b6f
gentbunjaku71 <gentbunjaku71@gmail.com>
2025-11-13 21:15:38 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import ProductListing from "./pages/ProductListing";
+import ShoppingCart from "./pages/ShoppingCart";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductListing />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
